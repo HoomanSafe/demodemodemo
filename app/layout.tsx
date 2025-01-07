3e53355980f044641a9c32cd8d490f9bd461c640
Hoo <hoomancanal@gmail.com>
2025-01-07 19:26:21 +0800

    update56

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Genius AI Chat',
+  title: 'OPERATORAI Chat',
   description: 'Experience automated intelligence',
 }
 
